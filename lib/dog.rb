4fde169ced118aab612c1a6f0556b645f878a3ac
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 05:55:14 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  
+  def bark
+    puts  "Woof!"
+  end
+end
+
+fido = Dog.new
+fido.bark
+
+  def sit
+    puts  "The Dog is sitting"
+ 
+end
+ 
+fido = Dog.new
+
+
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def talk
+    puts  "Hello World"
+  end
+end
+
+ada = Person.new
+ada.talk
+  
+  def walk
+    puts  "The Person is walking".
+  end
+end
+
+ada = Person.new
+
